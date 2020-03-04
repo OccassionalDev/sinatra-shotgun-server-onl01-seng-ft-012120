8c9476a19d1aa2b7f21455db832b1e1c98d37e75
Erica Schimmel <ericaschimmel01@gmail.com>
2020-03-04 20:34:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Hello there! I started my server using the Shotgun gem on ruby!"
   end
 
 end
